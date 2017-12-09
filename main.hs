b40ef670848444f57bd59d7b57ae91bc9ecfa2d8
Jakob Löw <jakob@m4gnus.de>
2017-12-09 14:04:20 +0100

    implement loop optimizations

--- main.hs
+++ main.hs
@@ -1,10 +1,13 @@
 import Data.List
 import Data.Maybe
+import Data.Foldable
 import qualified Data.Sequence as S
 import System.Environment
 
 data Statement
     = Math Int              -- *p += arg
+    | Set Int               -- *p = arg
+    | Copy Int Int Int      -- p[arg0] = *p * arg1 / arg2
     | Shift Int             -- p += arg
     | Loop [Statement]      -- while(*p) { arg }
     deriving(Show, Eq)
@@ -40,19 +43,49 @@ groupStatements statements                 = reverse $ foldl groupStatement [] s
 
 
 
+analyzeLoop (shift, condOp, mathOps) (Math x)
+    | shift == 0                                = (shift, condOp + x, mathOps)
+    | isJust prevOp                             = (shift, condOp, updatedSeq)
+    | otherwise                                 = (shift, condOp, (shift, x) S.<| mathOps)
+    where
+        prevOp                                  = S.findIndexL ((==shift) . fst) mathOps
+        prevOpIndex                             = fromJust prevOp
+        updatedVal                              = x + (snd $ S.index mathOps prevOpIndex)
+        updatedSeq                              = S.update prevOpIndex (shift, updatedVal) mathOps
+analyzeLoop (shift, condOp, mathOps) (Shift x)  = (shift + x, condOp, mathOps)
+analyzeLoop _ x                                 = error $ "analyzeLoop does not support statement " ++ (show x)
+
+copyFromMathOp condVal (offset, value)          = Copy offset value (-condVal)
+
+optimizeLoop loop@(Loop children)               = if hasChildLoops || totalShift /= 0
+    then [loop]
+    else (map (copyFromMathOp condVal) $ toList mathOps) ++ [(Set 0)]
+    where
+        children'                               = optimizeLoops children
+        hasChildLoops                           = any isLoop children'
+        (totalShift, condVal, mathOps)          = foldl analyzeLoop (0, 0, S.empty) children
+
+optimizeLoops statements                        = concat $ map optimizeIfLoop statements
+    where
+        optimizeIfLoop x                        = if isLoop x
+            then optimizeLoop x
+            else [x]
+
+
+
 optimizations :: [([Statement] -> [Statement])]
-optimizations = [groupStatements]
+optimizations = [groupStatements, optimizeLoops]
 
 main = do
     args            <- getArgs
     let mode        = args !! 0
 
     input           <- case mode of
-        "stdin"     -> readLn
+        "stdin"     -> getLine
         "file"      -> readFile $ args !! 1
         "arg"       -> return $ intercalate " " $ tail args
 
     let code        = parseStatements input
-        optimized   = map (flip ($) code) optimizations
+        optimized   = foldl (flip ($)) code optimizations
 
     putStrLn $ show optimized
