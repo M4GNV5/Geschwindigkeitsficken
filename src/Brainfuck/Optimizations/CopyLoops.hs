e197c28ee11231b69997f6bc929194e86c60c814
Jakob Löw <jakob@m4gnus.de>
2017-12-26 16:15:50 +0100

    tidy up copy loop code

--- src/Brainfuck/Optimizations/CopyLoops.hs
+++ src/Brainfuck/Optimizations/CopyLoops.hs
@@ -2,7 +2,7 @@ module Brainfuck.Optimizations.CopyLoops (optimizeLoops) where
 
 import Data.Maybe
 import Data.Foldable
-import qualified Data.Sequence as S
+import qualified Data.Map.Lazy as M
 
 import Brainfuck
 
@@ -16,26 +16,25 @@ isBasicOp _                 = False
 
 analyzeLoop (shift, condOp, mathOps) (Add off (Const val))
     | shift == 0 && off == 0                    = (shift, condOp + val, mathOps)
-    | isJust prevOp                             = (shift, condOp, updatedSeq)
-    | otherwise                                 = (shift, condOp, (target, val) S.<| mathOps)
+    | isJust prevOp                             = (shift, condOp, updatedMap)
+    | otherwise                                 = (shift, condOp, M.insert target val mathOps)
     where
         target                                  = shift + off
-        prevOp                                  = S.findIndexL ((==target) . fst) mathOps
-        prevOpIndex                             = fromJust prevOp
-        updatedVal                              = val + (snd $ S.index mathOps prevOpIndex)
-        updatedSeq                              = S.update prevOpIndex (target, updatedVal) mathOps
+        prevOp                                  = M.lookup target mathOps
+        updatedVal                              = val + fromJust prevOp
+        updatedMap                              = M.insert target updatedVal mathOps
 analyzeLoop (shift, condOp, mathOps) (Shift x)  = (shift + x, condOp, mathOps)
 analyzeLoop state (Comment _)                   = state
 
 optimizeLoop loop@(Loop off children)           = if hasNonBasicOps || totalShift /= 0 || condVal /= (-1)
     then [Loop off children']
-    else comments ++ optimizedChildren ++ [(Set 0 (Const 0))]
+    else comments ++ optimizedChildren ++ [(Set off (Const 0))]
     where
         children'                               = optimizeLoops children
         comments                                = filter isComment children
         hasNonBasicOps                          = any (not . isBasicOp) children'
-        (totalShift, condVal, mathOps)          = foldl analyzeLoop (0, 0, S.empty) children
-        optimizedChildren                       = map (\(off, val) -> Add off (Var 0 val)) $ toList mathOps
+        (totalShift, condVal, mathOps)          = foldl analyzeLoop (0, 0, M.empty) children
+        optimizedChildren                       = map (\(curr, val) -> Add curr (Var off val)) $ M.toList mathOps
 
 optimizeLoops statements                        = concat $ map optimizeIfLoop statements
     where
