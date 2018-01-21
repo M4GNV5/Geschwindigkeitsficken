31316288bd140ae8eb84905b71cea5a9ec21e14d
Jakob Löw <jakob@m4gnus.de>
2018-01-21 01:25:11 +0100

    fix a bug with -Ogroup which couldnt actually be triggered because of the order optimizations are applied

--- src/Brainfuck/Optimizations/Grouping.hs
+++ src/Brainfuck/Optimizations/Grouping.hs
@@ -2,9 +2,9 @@ module Brainfuck.Optimizations.Grouping (groupStatements) where
 
 import Brainfuck
 
-groupStatement ((Add off1 val1):rest) curr@(Add off2 val2)
+groupStatement stmts@((Add off1 val1):rest) curr@(Add off2 val2)
     | off1 == off2                              = (Add off1 $ addExpressions val1 val2) : rest
-    | otherwise                                 = curr : rest
+    | otherwise                                 = curr : stmts
 groupStatement ((Shift x):rest) (Shift y)       = (Shift $ x + y) : rest
 groupStatement ((Comment x):rest) (Comment y)   = (Comment $ x ++ y) : rest
 groupStatement rest (Loop off children)         = (Loop off $ groupStatements children) : rest
